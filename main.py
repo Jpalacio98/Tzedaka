182078d265245eea14718926811f8e1c52e9b18d
Jpalacio98 <135086340+Jpalacio98@users.noreply.github.com>
2023-08-14 22:26:09 -0500

    creacion-Jpalacio

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.uic import loadUi
+
+
+class Aplicacion(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.InicializarGUI()
+
+    def InicializarGUI(self):
+        #---------------------declaracion de variables-----------------------
+        self.ui=loadUi('Resource/Views/login.ui',self)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        pass
+    
+
+def main():
+    windows = QApplication(sys.argv)
+    ventana= Aplicacion()
+    ventana.show()
+
+    sys.exit(windows.exec_())
+
+if __name__ == "__main__":
+    main()
