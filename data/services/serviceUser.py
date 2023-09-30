655f8aaf6caa159972719504ea5b46643e196eb3
VictorArdila <valfonsoardila@unicesar.edu.co>
2023-09-30 16:23:37 -0500

    Firebase

--- data/services/serviceUser.py
+++ data/services/serviceUser.py
@@ -1,30 +1,46 @@
 import pyrebase
 
+
 class Peticioneslogin:
     def __init__(self):
         self.auth = pyrebase.auth()
-    
+
+        cred = {
+            "apiKey": "AIzaSyAnE6WxkeYDZ9Oro8moJUbTDWwML2VtdyY",
+            "authDomain": "tzedaka-db.firebaseapp.com",
+            "databaseURL": "https://tzedaka-db-default-rtdb.firebaseio.com",
+            "projectId": "tzedaka-db",
+            "storageBucket": "tzedaka-db.appspot.com",
+            "messagingSenderId": "818074652781",
+            "appId": "1:818074652781:web:d4f7c6b994745a5a59af64",
+            "measurementId": "G-0Y4L73XMB7",
+        }
+
+        self.firebase = pyrebase.initialize_app(cred)
+
     @staticmethod
     async def crear_registro_email(self, email, password):
         try:
             user = await self.auth.create_user(email=email, password=password)
             return user
         except pyrebase.auth.AuthError as e:
-            if e.code == 'weak-password':
-                return 'Contraseña Debil'
-            elif e.code == 'email-already-exists':
-                return 'Correo ya Existe'
+            if e.code == "weak-password":
+                return "Contraseña Debil"
+            elif e.code == "email-already-exists":
+                return "Correo ya Existe"
         except Exception as e:
             print(e)
 
     @staticmethod
     async def consultar_usuario(self):
         try:
-            user = await self.auth.get_user_by_uid('user_id')  # Reemplaza 'user_id' con el ID del usuario que deseas consultar
+            user = await self.auth.get_user_by_uid(
+                "user_id"
+            )  # Reemplaza 'user_id' con el ID del usuario que deseas consultar
             return user
         except self.auth.AuthError as e:
-            if e.code == 'user-not-found':
-                print('Usuario no encontrado')
+            if e.code == "user-not-found":
+                print("Usuario no encontrado")
             else:
                 print(e)
         except Exception as e:
@@ -33,16 +49,18 @@ class Peticioneslogin:
     @staticmethod
     async def ingresar_email(self, email, password):
         try:
-            user = await self.auth.sign_in_with_email_and_password(email=email, password=password)
+            user = await self.auth.sign_in_with_email_and_password(
+                email=email, password=password
+            )
             print(user.uid)
             return user
         except self.auth.AuthError as e:
-            if e.code == 'user-not-found':
-                print('Correo no encontrado')
-                return '1'
-            elif e.code == 'wrong-password':
-                print('Contraseña incorrecta')
-                return '2'
+            if e.code == "user-not-found":
+                print("Correo no encontrado")
+                return "1"
+            elif e.code == "wrong-password":
+                print("Contraseña incorrecta")
+                return "2"
             else:
                 print(e)
         except Exception as e:
@@ -59,7 +77,9 @@ class Peticioneslogin:
     @staticmethod
     async def abandonar_sesion(self):
         try:
-            await self.auth.revoke_refresh_tokens('user_id')  # Reemplaza 'user_id' con el ID del usuario
+            await self.auth.revoke_refresh_tokens(
+                "user_id"
+            )  # Reemplaza 'user_id' con el ID del usuario
             return "Sesión Cerrada"
         except Exception as e:
             print(e)
